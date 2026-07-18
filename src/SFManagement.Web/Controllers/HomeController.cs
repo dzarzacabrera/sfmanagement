5870902984fcf26f7c5c4b62d5e6531bbee04be4
dzarzacabrera <davidzarzacabrera@gmail.com>
2026-07-18 23:34:33 +0200

    Landing page y mejoras visuales en modales
    Se añade una landing page moderna en Index.cshtml con nuevo layout (_LandingLayout.cshtml), header y footer personalizados. HomeController ahora muestra la landing en vez de redirigir. Se mejoran paddings y cabeceras en el modal de evaluación y se amplía el ancho máximo del modal principal para mejor visualización en pantallas grandes.

--- src/SFManagement.Web/Controllers/HomeController.cs
+++ src/SFManagement.Web/Controllers/HomeController.cs
@@ -7,6 +7,6 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         ViewBag.PageTitle = "Home";
-        return RedirectToAction("Index", "Project");
+        return View();
     }
 }
